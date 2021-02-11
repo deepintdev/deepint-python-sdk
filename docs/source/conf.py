3eb85956eaebaedec899094696445ba07b8b1c28
Francisco Pinto Santos <franpintosantos@usal.es>
2021-02-11 15:48:46 +0100

    Fixed doc requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Deep Intelligence SDK for Python'
-copyright = '2021 DeepIntelligence'
+copyright = '2021 Deep Intelligence'
 author = 'Deep Intelligence'
 
 # The full version, including alpha/beta/rc tags
@@ -90,7 +90,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
-html_static_path = []
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
